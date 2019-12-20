5dc36b709fa8bccd460087f82dda46718189f671
Jorge Orozco <jorozco@werfen.com>
2019-12-20 16:21:25 +0100

    Solve issues with allure running integration tests

---
+++ src/IntegrationTest/AllureConfigSetUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntegrationTest
+{
+    using System.IO;
+
+    using Allure.Commons;
+
+    using NUnit.Framework;
+
+    [SetUpFixture]
+    public class AllureConfigSetUp
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            Environment.SetEnvironmentVariable(
+                "ALLURE_CONFIG",
+                Path.Combine(Environment.CurrentDirectory, AllureConstants.CONFIG_FILENAME));
+        }
+    }
+}
